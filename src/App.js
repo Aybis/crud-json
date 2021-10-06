c159f32ed7251214368a99a6168b20f3596c7baf
Aybis <muchtara2m@gmail.com>
2021-10-06 11:17:08 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import Data from "./data.json";
+import Data from './data.json';
 import { v1 as uuidv1 } from 'uuid';
 import axios from 'axios';
 
 function App() {
-
   // Reference
   const titleRef = useRef();
   const contentRef = useRef();
 
-  // State 
+  // State
   const [data, setData] = useState(Data);
 
   // Temp State
@@ -29,20 +28,18 @@ function App() {
     // clear form fields
     titleRef.current.value = null;
     contentRef.current.value = null;
-  },[data]);
-  
-
+  }, [data]);
 
   // Add Post
   //////////////////////////////////////////
   const addPost = () => {
-    if(title && content) {
+    if (title && content) {
       // create new post object
       let newPost = {
-        "id": uuidv1(),
-        "title": title,
-        "content": content
-      }
+        id: uuidv1(),
+        title: title,
+        content: content,
+      };
       // merge new post with copy of old state
       let posts = [...data, newPost];
       // push new object to state
@@ -53,44 +50,40 @@ function App() {
 
       // update write to json file
       saveJson(posts);
+    }
+  };
 
-   }
-  }
-  
-
-
-  // Delete Post 
+  // Delete Post
   //////////////////////////////////////////
   const deletePost = (key) => {
     // filter out post containing that id
-    let filterOutPost = [...data].filter(OBJ=>OBJ.id!==key);
+    let filterOutPost = [...data].filter((OBJ) => OBJ.id !== key);
     // save the rest in state
     setData(filterOutPost);
 
     // update write to json file
     saveJson(filterOutPost);
-
-  }
+  };
 
   // Populate Post
-  ////////////////////////////////////////// 
+  //////////////////////////////////////////
   const populatePost = (key, title, content) => {
     setUpdateID(key);
     setUpdateTitle(title);
     setUpdateContent(content);
-  }
+  };
 
-  // Update Post 
+  // Update Post
   //////////////////////////////////////////
   const updatePost = () => {
     // populate post info from temp state and prepare new object for changed post
     let editedPost = {
-      "id": updateID,
-      "title": updateTitle,
-      "content": updateContent
-    }
-    // remove old post with same ID and get the remaining data /// filter 
-    let filterPost = [...data].filter(OBJ=>OBJ.id!==updateID);
+      id: updateID,
+      title: updateTitle,
+      content: updateContent,
+    };
+    // remove old post with same ID and get the remaining data /// filter
+    let filterPost = [...data].filter((OBJ) => OBJ.id !== updateID);
     // prepare object with edited post + remaining data from object above
     let posts = [...filterPost, editedPost];
     // push int state
@@ -102,28 +95,26 @@ function App() {
 
     // update write to json file
     saveJson(posts);
-
-  }
+  };
 
   // Write to JSON File
   //////////////////////////////////////////
   // this function will receive all uodated state / posts after you add, edit delete post
   const saveJson = (posts) => {
     // api URL // end point from node server / express server
-    const url = 'http://localhost:5000/write'
-    axios.post(url, posts)
-    .then(response => {
+    const url = 'https://test-crud.vercel.app:5000/write';
+    axios.post(url, posts).then((response) => {
       // console.log(response);
     });
-  }
+  };
 
   // Bonus Section
   //////////////////////////////////////////
   // Downloading JSON File
-  const saveData = jsonDate => {
+  const saveData = (jsonDate) => {
     const fileData = JSON.stringify(jsonDate);
 
-    const blob = new Blob([fileData], {type: "text/plain"});
+    const blob = new Blob([fileData], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
     // create link
     const link = document.createElement('a');
@@ -132,65 +123,68 @@ function App() {
     link.href = url;
     // trigger download
     link.click();
-  }
-
-
+  };
 
   return (
     <div className="App">
-
       <div>
         <h4>Add New Post</h4>
-        <input placeholder="Title" 
-          onChange={ e => setTitle( e.target.value ) } 
-          value={ title || '' } 
-          ref={ titleRef }
+        <input
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title || ''}
+          ref={titleRef}
         />
         <br />
-        <textarea 
+        <textarea
           placeholder="Content"
-          onChange={ e => setContent( e.target.value ) } 
-          value={ content || '' } 
-          ref={ contentRef }
-        ></textarea>
+          onChange={(e) => setContent(e.target.value)}
+          value={content || ''}
+          ref={contentRef}></textarea>
         <br />
-        <button onClick={ addPost }>Add Post</button>
+        <button onClick={addPost}>Add Post</button>
       </div>
 
       {/* If temp state has got values of title and content for update form show this */}
 
-      { updateTitle || updateContent ? 
-        (
-          <div>
-            <h4>Update Post</h4>
-            <input placeholder="Title" 
-              onChange={ e => setUpdateTitle( e.target.value ) } 
-              value={ updateTitle || '' } 
-            />
-            <br />
-            <textarea
-              placeholder="Content"
-              onChange={ e => setUpdateContent( e.target.value ) } 
-              value={ updateContent || '' } 
-            ></textarea>
-            <br />
-            <button onClick={ updatePost }>Update Post</button>
-          </div>
-        ) : null }
+      {updateTitle || updateContent ? (
+        <div>
+          <h4>Update Post</h4>
+          <input
+            placeholder="Title"
+            onChange={(e) => setUpdateTitle(e.target.value)}
+            value={updateTitle || ''}
+          />
+          <br />
+          <textarea
+            placeholder="Content"
+            onChange={(e) => setUpdateContent(e.target.value)}
+            value={updateContent || ''}></textarea>
+          <br />
+          <button onClick={updatePost}>Update Post</button>
+        </div>
+      ) : null}
 
       <div className="posts">
-        { data ? data.map(post => {
-          return(
-            <div key={ post.id } className="post">
-              <h3>{ post.title }</h3>
-              <p>{ post.content }</p>
-              <button onClick={ () => populatePost(post.id, post.title, post.content) }>Edit</button>
-              <button onClick={ () => deletePost(post.id) }>Delete</button>
-            </div>
-          )
-        }) : null }
+        {data
+          ? data.map((post) => {
+              return (
+                <div key={post.id} className="post">
+                  <h3>{post.title}</h3>
+                  <p>{post.content}</p>
+                  <button
+                    onClick={() =>
+                      populatePost(post.id, post.title, post.content)
+                    }>
+                    Edit
+                  </button>
+                  <button onClick={() => deletePost(post.id)}>Delete</button>
+                </div>
+              );
+            })
+          : null}
         <div className="btn-download">
-          <button onClick={ e => saveData(data) }>Download Data</button>
+          <button onClick={(e) => saveData(data)}>Download Data</button>
         </div>
       </div>
     </div>
